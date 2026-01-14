35f1552c4fb04598f5287bc5c278b41b38c2b2cb
P-cyber162 <anakwaprince204@gmail.com>
2026-01-14 01:11:44 +0000

    user model defined

---
+++ src/models/userModel.ts
@@ -0,0 +1,67 @@
+import mongoose, { Document, Schema } from 'mongoose';
+import bcryptjs from 'bcryptjs';
+import validator from 'validator';
+import { timeStamp } from 'node:console';
+
+export interface IUser extends Document {
+    username: string,
+    email: string,
+    password: string,
+    confirmPassword: string | undefined,
+    role: string,
+    createdAt: Date,
+    updatedAt: Date,
+    resetPasswordToken: string,
+    resetPassswordExpires: Date,
+};
+
+const userSchema = new Schema<IUser>({
+    username: {
+        type:String,
+        trim: true,
+        unique: true,
+        required: [true, 'A username is required!']
+    },
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        required: [true, 'An email is required!'],
+        validate: {
+            validator: function(value: string) {
+                return validator.isEmail(value)
+            },
+            message: 'Invalid Email!'
+        }
+    },
+    password: {
+        type: String,
+        select: false,
+        required: [true, 'A password is required!']
+    },
+    confirmPassword: {
+        type: String,
+        required: [true, 'Please confirm your password!'],
+        validate: {
+            validator: function (this: IUser, value) {
+                return value === this.password
+            },
+            message: 'Password does not match!'
+        }
+    },
+    role: {
+        type:String,
+        enum: ['user', 'admin', 'guest'],
+        default: 'user'
+    }
+}, { timestamps: true });
+
+// HASSING PASSWORD 
+userSchema.pre('save', async function() {
+    if(this.isModified('password')) {
+        this.password = await bcryptjs.hash(this.password, 12);
+    }
+    this.confirmPassword = undefined;
+});
+
+export const User = mongoose.model<IUser>('User', userSchema);
