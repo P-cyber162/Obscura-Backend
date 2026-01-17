5700fd7ac7c4b4eb3f729e815f23dbdaf459e026
P-cyber162 <anakwaprince204@gmail.com>
2026-01-17 00:43:20 +0000

    rate limitter set up

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import morgan from 'morgan';
 import authRoutes from './routes/authRoutes';
 import userRoutes from './routes/userRoutes';
 import { createApiLimitter } from './middleware/rateLimmtter';
@@ -11,7 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 const apiLimitter = createApiLimitter();
 
 app.use('/api', apiLimitter);
-app.use();
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,22 +1,26 @@
 import express from 'express';
 import { signUp, login, forgotPassword, resetPassword } from '../controllers/authController';
+import { createAuthLimitter, createPasswordResetLimitter } from '../middleware/rateLimmtter';
 
 const router = express.Router();
 
+const authLimitter = createAuthLimitter();
+const passwordLimitter = createPasswordResetLimitter;
+
 router
     .route('/register')
-    .post(signUp);
+    .post(authLimitter, signUp);
 
 router  
     .route('/login')
-    .post(login);
+    .post(authLimitter, login);
 
 router
     .route('/forgotPassword')
-    .post(forgotPassword);
+    .post(passwordLimitter, forgotPassword);
 
 router
     .route('/resetPassword')
-    .post(resetPassword);
+    .post(passwordLimitter, resetPassword);
 
 export default router;
