33d80e83dc4006704739a4a40f598f60c99f91f0
P-cyber162 <anakwaprince204@gmail.com>
2026-01-18 22:00:53 +0000

    OAuth set up

--- src/models/allbulmModel.ts
+++ src/models/allbulmModel.ts
@@ -1,1 +1,35 @@
 import mongoose, {Schema, Document} from 'mongoose';
+
+export interface IAlbum extends Document {
+    name: string;
+    description?: string;
+    owner: mongoose.Types.ObjectId;
+    photos: mongoose.Types.ObjectId[];
+    createdAt: Date;
+    updatedAt: Date;
+};
+
+const albumSchema = new Schema<IAlbum>({
+    name: {
+        type: String,
+        required: [true, 'An album is required!'],
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Album must belong to a user!']
+    },
+    photos: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'Photo'
+    }]
+}, {timestamps: true});
+
+albumSchema.index({ owner: 1 });
+
+export const Album = mongoose.model<IAlbum>('Album', albumSchema);
