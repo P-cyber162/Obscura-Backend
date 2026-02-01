734dcf77d6a7d1724fe836ee38b3e56f22caedb6
P-cyber162 <anakwaprince204@gmail.com>
2026-02-01 23:43:40 +0000

    Obscura Completed!

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -1,7 +1,7 @@
 import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
 const cloudName= process.env.CLOUDINARY_CLOUD_NAME;
 const apiKey= process.env.CLOUDINARY_API_KEY;
