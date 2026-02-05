45a800b5a03e4dbab772a346c916d90092436460
P-cyber162 <anakwaprince204@gmail.com>
2026-02-05 22:19:46 +0000

    Revert "fix(ratelimiter): Fixed the ratelimiter import"
    This reverts commit 5fe8ebeadedb619b000ca569cb43f207cb1c2a49.

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -1,6 +1,6 @@
 import rateLimit from "express-rate-limit";
 import RedisStore from "rate-limit-redis";
-import redisClient from "../config/redis.js";
+import redisClient from "../config/redis";
 
 export const createApiLimitter = () => rateLimit({
     store: new RedisStore ({
